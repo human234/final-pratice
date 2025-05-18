cbbb4cba64fbd298f892f2ccb043438d0b14fd6f
KamuiFan <fanyunxiang65@gmail.com>
2025-05-18 15:52:46 +0800

    速度調整 背景音樂
    速度調整為每7個磚塊加速-2msec delay
    背景音樂加入
    gameover音效加入
    將磚塊總數調整為5*9=45個

--- src/SoundManager.java
+++ src/SoundManager.java
@@ -6,9 +6,7 @@ public class SoundManager {
 
     private static Clip backgroundClip;
 
-    /**
-     * 播放背景音樂（會自動 loop）
-     */
+    //播放一次性音效
     public static void playSoundEffect(String path, float volumeDb) {
         new Thread(() -> {
             try {
@@ -20,6 +18,27 @@ public class SoundManager {
                     volumeControl.setValue(volumeDb); // 直接用參數控制音量
                 }
                 clip.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+    /**
+     * 播放背景音樂（會自動 loop）
+     */
+    public static void playBackgroundMusic(String path, float volumeDb) {
+        stopBackgroundMusic(); // 先停止舊的背景音樂
+        new Thread(() -> {
+            try {
+                AudioInputStream ais = AudioSystem.getAudioInputStream(new File(path));
+                backgroundClip = AudioSystem.getClip();
+                backgroundClip.open(ais);
+                if (backgroundClip.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+                    FloatControl volumeControl = (FloatControl) backgroundClip.getControl(FloatControl.Type.MASTER_GAIN);
+                    volumeControl.setValue(volumeDb);
+                }
+                backgroundClip.loop(Clip.LOOP_CONTINUOUSLY); // 循環播放
+                backgroundClip.start();
             } catch (Exception e) {
                 e.printStackTrace();
             }
